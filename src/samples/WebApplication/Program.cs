e2c79b043a8f6e252e6b759a044499c753807fae
Silvio Greuel <silviogreuel@gmail.com>
2019-10-01 20:42:50 -0300

    Adding docker-compose and Dockerfile to support some sample usage

--- src/samples/WebApplication/Program.cs
+++ src/samples/WebApplication/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
