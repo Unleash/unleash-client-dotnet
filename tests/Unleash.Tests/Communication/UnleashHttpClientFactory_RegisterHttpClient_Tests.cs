e9af9168c6263c9debc5e9f63ba4a3d8c9b8e0f4
amj1985 <amj1985@live.com>
2019-10-29 20:21:38 +0100

    (STYLE) Added missing space

--- tests/Unleash.Tests/Communication/UnleashHttpClientFactory_RegisterHttpClient_Tests.cs
+++ tests/Unleash.Tests/Communication/UnleashHttpClientFactory_RegisterHttpClient_Tests.cs
@@ -10,6 +10,7 @@ namespace Unleash.Tests.Communication
         private Uri _newApiUri { get; } = new Uri("http://unleash2.herokuapp2.com/");
 
         private DefaultHttpClientFactory _httpClientFactory { get; } = new DefaultHttpClientFactory();
+
         [Test]
         public void HttpClientFactory_Should_Create_Single_Instance_By_Dns_Success()
         {
