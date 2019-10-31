ab3fc6270082f357f802d00569c380283cc7eea0
amj1985 <amj1985@live.com>
2019-10-31 08:53:42 +0100

    (STYLE) Fixed code style

--- tests/Unleash.Tests/Communication/UnleashHttpClientFactory_RegisterHttpClient_Tests.cs
+++ tests/Unleash.Tests/Communication/UnleashHttpClientFactory_RegisterHttpClient_Tests.cs
@@ -5,20 +5,19 @@ namespace Unleash.Tests.Communication
 {
     public class UnleashHttpClientFactory_RegisterHttpClient_Tests
     {
-        private Uri _apiUri { get; } = new Uri("http://unleash.herokuapp.com/");
+        private readonly Uri apiUri = new Uri("http://unleash.herokuapp.com/");
 
-        private Uri _newApiUri { get; } = new Uri("http://unleash2.herokuapp2.com/");
+        private readonly Uri newApiUri = new Uri("http://unleash2.herokuapp2.com/");
 
         private DefaultHttpClientFactory _httpClientFactory { get; } = new DefaultHttpClientFactory();
 
         [Test]
         public void HttpClientFactory_Should_Create_Single_Instance_By_Dns_Success()
         {
-
-            var httpClient = _httpClientFactory.Create(_apiUri);
+            var httpClient = _httpClientFactory.Create(apiUri);
             var expectedHashId = httpClient.GetHashCode();
 
-            var newHttpClient = _httpClientFactory.Create(_apiUri);
+            var newHttpClient = _httpClientFactory.Create(apiUri);
             var actualHashId = newHttpClient.GetHashCode();
 
             Assert.AreEqual(expectedHashId, actualHashId);
@@ -27,11 +26,10 @@ namespace Unleash.Tests.Communication
         [Test]
         public void HttpClientFactory_Should_Create_One_Instance_Per_Dns_Success()
         {
-
-            var httpClient = _httpClientFactory.Create(_apiUri);
+            var httpClient = _httpClientFactory.Create(apiUri);
             var expectedHashId = httpClient.GetHashCode();
 
-            var newHttpClient = _httpClientFactory.Create(_newApiUri);
+            var newHttpClient = _httpClientFactory.Create(newApiUri);
             var actualHashId = newHttpClient.GetHashCode();
 
             Assert.AreNotEqual(expectedHashId, actualHashId);
