bdf8a839fcfe904e1c10243000a9583e72615bb7
David Leek <davidekl@hotmail.com>
2022-01-05 23:59:00 +0100

    chore: add some more CachedFilesLoader tests

--- tests/Unleash.Tests/Internal/CachedFilesLoader_Tests.cs
+++ tests/Unleash.Tests/Internal/CachedFilesLoader_Tests.cs
@@ -16,16 +16,84 @@ namespace Unleash.Tests.Internal
         }
 
         [Test]
-        public void Sets_Etag_From_Etag_File_When_Backup_Is_Not_Empty()
+        public void Sets_Etag_From_Etag_File_And_Toggles_From_Backup_When_Backup_Is_Not_Empty()
         {
+            // Arrange
             string toggleFileName = AppDataFile("unleash-repo-v1.json");
             string etagFileName = AppDataFile("etag-12345.txt");
             var serializer = new JsonNetSerializer();
             var fileSystem = new FileSystem(Encoding.UTF8);
-
             var fileLoader = new CachedFilesLoader(serializer, fileSystem, toggleFileName, etagFileName);
+
+            // Act
             var ensureResult = fileLoader.EnsureExistsAndLoad();
+
+            // Assert
             ensureResult.InitialETag.Should().Be("12345");
+            ensureResult.InitialToggleCollection.Features.Should().HaveCount(3);
+        }
+
+        [Test]
+        public void Writes_Empty_Etag_File_And_Sets_Etag_To_Empty_String_And_Loads_Toggles_When_Etag_File_Is_Missing_But_Backup_Exists()
+        {
+            // Arrange
+            string toggleFileName = AppDataFile("unleash-repo-v1.json");
+            string etagFileName = AppDataFile("etag-missing.txt");
+            var serializer = new JsonNetSerializer();
+            var fileSystem = new FileSystem(Encoding.UTF8);
+            var fileLoader = new CachedFilesLoader(serializer, fileSystem, toggleFileName, etagFileName);
+
+            // Act
+            var ensureResult = fileLoader.EnsureExistsAndLoad();
+
+            // Assert
+            ensureResult.InitialETag.Should().Be(string.Empty);
+            fileSystem.FileExists(etagFileName).Should().BeTrue();
+            fileSystem.ReadAllText(etagFileName).Should().Be(string.Empty);
+            ensureResult.InitialToggleCollection.Features.Should().HaveCount(3);
+        }
+
+        [Test]
+        public void Sets_Etag_To_Empty_String_And_Toggles_To_Null_When_Etag_File_Exists_But_Backup_File_Does_Not()
+        {
+            // Arrange
+            string toggleFileName = AppDataFile("unleash-repo-missing.json");
+            string etagFileName = AppDataFile("etag-12345.txt");
+            var serializer = new JsonNetSerializer();
+            var fileSystem = new FileSystem(Encoding.UTF8);
+            var fileLoader = new CachedFilesLoader(serializer, fileSystem, toggleFileName, etagFileName);
+
+            // Act
+            var ensureResult = fileLoader.EnsureExistsAndLoad();
+
+            // Assert
+            ensureResult.InitialETag.Should().Be(string.Empty);
+            ensureResult.InitialToggleCollection.Should().BeNull();
+            fileSystem.FileExists(toggleFileName).Should().BeTrue();
+            fileSystem.ReadAllText(toggleFileName).Should().Be(string.Empty);
+        }
+
+        [Test]
+        public void Sets_Etag_To_Empty_String_And_Toggles_To_Null_When_Neither_File_Exists()
+        {
+            // Arrange
+            string toggleFileName = AppDataFile("unleash-repo-missing.json");
+            string etagFileName = AppDataFile("etag-missing.txt");
+            var serializer = new JsonNetSerializer();
+            var fileSystem = new FileSystem(Encoding.UTF8);
+            var fileLoader = new CachedFilesLoader(serializer, fileSystem, toggleFileName, etagFileName);
+
+            // Act
+            var ensureResult = fileLoader.EnsureExistsAndLoad();
+
+            // Assert
+            ensureResult.InitialETag.Should().Be(string.Empty);
+            fileSystem.FileExists(etagFileName).Should().BeTrue();
+            fileSystem.ReadAllText(etagFileName).Should().Be(string.Empty);
+
+            ensureResult.InitialToggleCollection.Should().BeNull();
+            fileSystem.FileExists(toggleFileName).Should().BeTrue();
+            fileSystem.ReadAllText(toggleFileName).Should().Be(string.Empty);
         }
     }
 }
