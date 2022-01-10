64b23a682861725b3221a3778dc761d81c6ad33c
David Leek <davidekl@hotmail.com>
2022-01-11 00:13:56 +0100

    implement togglebootstrapfileprovider

---
+++ src/Unleash/Utilities/ToggleBootstrapFileProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unleash.Internal;
+
+namespace Unleash.Utilities
+{
+    public class ToggleBootstrapFileProvider : IToggleBootstrapProvider
+    {
+        private readonly string filePath;
+        private readonly IFileSystem fileSystem;
+
+        public ToggleBootstrapFileProvider(string filePath)
+        {
+            this.filePath = filePath;
+            fileSystem = new FileSystem(Encoding.UTF8);
+        }
+
+        internal ToggleBootstrapFileProvider(string filePath, IFileSystem fileSystem)
+        {
+            this.filePath = filePath;
+            this.fileSystem = fileSystem;
+        }
+
+        public string Read()
+        {
+            if (!fileSystem.FileExists(filePath))
+                return string.Empty;
+
+            return fileSystem.ReadAllText(filePath);
+        }
+    }
+}
---
+++ tests/Unleash.Tests/Utilities/ToggleBootstrapFileProvider_Tests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unleash.Internal;
+using Unleash.Utilities;
+
+namespace Unleash.Tests.Utilities
+{
+    public class ToggleBootstrapFileProvider_Tests
+    {
+        protected string AppDataFile(string filename)
+        {
+            var file = Path.Combine(TestContext.CurrentContext.TestDirectory, "App_Data", filename);
+            return file;
+        }
+
+        [Test]
+        public void Returns_String_Empty_When_File_Does_Not_Exist()
+        {
+            // Arrange
+            var fileSystem = new FileSystem(Encoding.UTF8);
+            string toggleFileName = AppDataFile("unleash-repo-v1-missing.json");
+            var toggleFileProvider = new ToggleBootstrapFileProvider(toggleFileName, fileSystem);
+
+            // Act
+            var emptyResult = toggleFileProvider.Read();
+
+            // Assert
+            emptyResult.Should().Equals(string.Empty);
+        }
+
+        [Test]
+        public void Returns_File_Content_When_File_Exists()
+        {
+            // Arrange
+            var fileSystem = new FileSystem(Encoding.UTF8);
+            string toggleFileName = AppDataFile("unleash-repo-v1.json");
+            var toggleFileProvider = new ToggleBootstrapFileProvider(toggleFileName, fileSystem);
+            var fileContent = fileSystem.ReadAllText(toggleFileName);
+
+            // Act
+            var result = toggleFileProvider.Read();
+
+            // Assert
+            result.Should().Equals(fileContent);
+        }
+    }
+}
