453a8d8f04957caa2982767d8f3b0c1d348a3571
David Leek <davidekl@hotmail.com>
2022-02-07 22:49:46 +0100

    feat: IConstraintOperator

---
+++ src/Unleash/Strategies/Constraints/IConstraintOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unleash.Internal;
+
+namespace Unleash.Strategies.Constraints
+{
+    public interface IConstraintOperator
+    {
+        bool Evaluate(Constraint constraint, UnleashContext context);
+    }
+}
