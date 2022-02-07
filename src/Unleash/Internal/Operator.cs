1a510264812c17e2ea02cd4f3ad29ad6964223c4
David Leek <davidekl@hotmail.com>
2022-02-07 22:49:14 +0100

    feat: new operators

--- src/Unleash/Internal/Operator.cs
+++ src/Unleash/Internal/Operator.cs
@@ -7,6 +7,19 @@ namespace Unleash.Internal
     public enum Operator
     {
         IN,
-        NOT_IN
+        NOT_IN,
+        STR_ENDS_WITH,
+        STR_STARTS_WITH,
+        STR_CONTAINS,
+        NUM_EQ,
+        NUM_GT,
+        NUM_GTE,
+        NUM_LT,
+        NUM_LTE,
+        DATE_AFTER,
+        DATE_BEFORE,
+        SEMVER_EQ,
+        SEMVER_GT,
+        SEMVER_LT
     }
 }
