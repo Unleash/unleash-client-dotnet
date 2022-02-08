0a4c87f5ee3b368121dbfeb333f8c5e60c689b80
David Leek <davidekl@hotmail.com>
2022-02-08 23:39:35 +0100

    numberconstraintoperator tests

---
+++ src/Unleash/Strategies/Constraints/NumberConstraintOperator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unleash.Internal;
+
+namespace Unleash.Strategies.Constraints
+{
+    public class NumberConstraintOperator : IConstraintOperator
+    {
+        public bool Evaluate(Constraint constraint, UnleashContext context)
+        {
+
+        }
+    }
+}
