7b31d4196e5399a46635c08484f52fbb4eef1895
David Leek <davidekl@hotmail.com>
2022-02-09 01:13:58 +0100

    implement numberconstraintoperator

--- src/Unleash/Strategies/Constraints/NumberConstraintOperator.cs
+++ src/Unleash/Strategies/Constraints/NumberConstraintOperator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Unleash.Internal;
 
@@ -9,7 +10,35 @@ namespace Unleash.Strategies.Constraints
     {
         public bool Evaluate(Constraint constraint, UnleashContext context)
         {
+            var contextValueString = context.GetByName(constraint.ContextName);
+            if (string.IsNullOrWhiteSpace(contextValueString))
+                return false;
 
+            if (!double.TryParse(contextValueString, out var contextValue))
+                return false;
+
+            return constraint.Values?.Select(val =>
+                double.TryParse(val, out var constraintVal) ? (double?)constraintVal : null
+            ).Any(val => val.HasValue && Eval(constraint.Operator, val.Value, contextValue)) ?? false;
+        }
+
+        private bool Eval(Operator @operator, double constraintValue, double contextValue)
+        {
+            switch (@operator)
+            {
+                case Operator.NUM_EQ:
+                    return contextValue == constraintValue;
+                case Operator.NUM_GT:
+                    return contextValue > constraintValue;
+                case Operator.NUM_GTE:
+                    return contextValue >= constraintValue;
+                case Operator.NUM_LT:
+                    return contextValue < constraintValue;
+                case Operator.NUM_LTE:
+                    return contextValue <= constraintValue;
+                default:
+                    return false;
+            }
         }
     }
 }
