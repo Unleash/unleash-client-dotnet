cc106aa2ce68da30d5ac44768d323e7c4040fa2c
David Leek <davidekl@hotmail.com>
2022-02-10 00:19:48 +0100

    feat: add date constraint operator and tests

---
+++ src/Unleash/Strategies/Constraints/DateConstraintOperator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unleash.Internal;
+
+namespace Unleash.Strategies.Constraints
+{
+    public class DateConstraintOperator : IConstraintOperator
+    {
+        public bool Evaluate(Constraint constraint, UnleashContext context)
+        {
+            if (string.IsNullOrWhiteSpace(constraint.Value) || !DateTimeOffset.TryParse(constraint.Value, out var constraintDate))
+                return false;
+
+            var contextValue = context.GetByName(constraint.ContextName);
+            if (string.IsNullOrWhiteSpace(contextValue) || !DateTimeOffset.TryParse(contextValue, out var contextDate))
+                return false;
+
+            return Eval(constraint.Operator, constraintDate, contextDate);
+        }
+
+        private bool Eval(Operator @operator, DateTimeOffset constraintDate, DateTimeOffset contextDate)
+        {
+            if (@operator == Operator.DATE_AFTER)
+                return contextDate > constraintDate;
+
+            if (@operator == Operator.DATE_BEFORE)
+                return contextDate < constraintDate;
+
+            return false;
+        }
+    }
+}
---
+++ tests/Unleash.Tests/Strategy/Constraints/DateConstraintOperator_Tests.cs
@@ -0,0 +1,116 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unleash.Internal;
+using Unleash.Strategies.Constraints;
+
+namespace Unleash.Tests.Strategy.Constraints
+{
+    public class DateConstraintOperator_Tests
+    {
+        [Test]
+        public void DATE_AFTER_2_Hours_Later_Is_After()
+        {
+
+            // Arrange
+            var target = new DateConstraintOperator();
+            var constraint = new Constraint("event_date", "2022-01-29T13:00:00.000Z", Operator.DATE_AFTER);
+            var context = new UnleashContext();
+            context.Properties.Add("event_date", "2022-01-29T15:00:00.000Z");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void DATE_AFTER_Same_Time_Is_Not_After()
+        {
+
+            // Arrange
+            var target = new DateConstraintOperator();
+            var constraint = new Constraint("event_date", "2022-01-29T13:00:00.000Z", Operator.DATE_AFTER);
+            var context = new UnleashContext();
+            context.Properties.Add("event_date", "2022-01-29T13:00:00.000Z");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void DATE_AFTER_2_Hours_Before_Is_Not_After()
+        {
+
+            // Arrange
+            var target = new DateConstraintOperator();
+            var constraint = new Constraint("event_date", "2022-01-29T13:00:00.000Z", Operator.DATE_AFTER);
+            var context = new UnleashContext();
+            context.Properties.Add("event_date", "2022-01-29T11:00:00.000Z");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void DATE_BEFORE_2_Hours_Earlier_Is_Before()
+        {
+
+            // Arrange
+            var target = new DateConstraintOperator();
+            var constraint = new Constraint("event_date", "2022-01-29T13:00:00.000Z", Operator.DATE_BEFORE);
+            var context = new UnleashContext();
+            context.Properties.Add("event_date", "2022-01-29T11:00:00.000Z");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void DATE_AFTER_Same_Time_Is_Not_Before()
+        {
+
+            // Arrange
+            var target = new DateConstraintOperator();
+            var constraint = new Constraint("event_date", "2022-01-29T13:00:00.000Z", Operator.DATE_BEFORE);
+            var context = new UnleashContext();
+            context.Properties.Add("event_date", "2022-01-29T13:00:00.000Z");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+        [Test]
+        public void DATE_AFTER_2_Hours_Later_Is_Not_Before()
+        {
+
+            // Arrange
+            var target = new DateConstraintOperator();
+            var constraint = new Constraint("event_date", "2022-01-29T13:00:00.000Z", Operator.DATE_BEFORE);
+            var context = new UnleashContext();
+            context.Properties.Add("event_date", "2022-01-29T15:00:00.000Z");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
