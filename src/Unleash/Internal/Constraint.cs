a3c2459c68cc25b0332f69d652d85122936fb6e9
David Leek <davidekl@hotmail.com>
2022-02-10 00:19:14 +0100

    add single value property to constraint

--- src/Unleash/Internal/Constraint.cs
+++ src/Unleash/Internal/Constraint.cs
@@ -9,12 +9,19 @@ namespace Unleash.Internal
         public string ContextName { get; private set; }
         public Operator Operator { get; private set; }
         public string[] Values { get; private set; }
+        public string Value { get; internal set; }
 
         public Constraint(string contextName, Operator @operator, params string[] values)
         {
             ContextName = contextName;
             Operator = @operator;
             Values = values;
+        }
+        public Constraint(string contextName, string value, Operator @operator)
+        {
+            ContextName = contextName;
+            Operator = @operator;
+            Value = value;
         }
     }
 }
