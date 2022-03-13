8b2033e20947f705886f95c54d2efe8ecf049dc7
David Leek <davidekl@hotmail.com>
2022-03-13 23:43:56 +0100

    feat: stringconstraintoperator and tests

---
+++ src/Unleash/Strategies/Constraints/StringConstraintOperator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unleash.Internal;
+
+namespace Unleash.Strategies.Constraints
+{
+    public class StringConstraintOperator : IConstraintOperator
+    {
+        public bool Evaluate(Constraint constraint, UnleashContext context)
+        {
+            if (string.IsNullOrWhiteSpace(constraint.Value))
+                return false;
+
+            var contextValue = context.GetByName(constraint.ContextName);
+            if (string.IsNullOrWhiteSpace(contextValue))
+                return false;
+
+            return Eval(constraint.Operator, constraint.Value, contextValue, constraint.CaseInsensitive);
+        }
+
+        private bool Eval(Operator @operator, string value, string contextValue, bool caseInsensitive)
+        {
+            var comparison = caseInsensitive ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+
+            if (@operator == Operator.STR_CONTAINS)
+                return contextValue.IndexOf(value, comparison) > -1;
+
+            if (@operator == Operator.STR_ENDS_WITH)
+                return contextValue.EndsWith(value, comparison);
+
+            if (@operator == Operator.STR_STARTS_WITH)
+                return contextValue.StartsWith(value, comparison);
+
+            return false;
+        }
+    }
+}
---
+++ tests/Unleash.Tests/Strategy/Constraints/StringConstraintOperator_Tests.cs
@@ -0,0 +1,219 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unleash.Internal;
+using Unleash.Strategies.Constraints;
+
+namespace Unleash.Tests.Strategy.Constraints
+{
+    public class StringConstraintOperator_Tests
+    {
+        [Test]
+        public void STR_CONTAINS_CaseInsensitive_False_Matches_With_Same_Case()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "sentence", Operator.STR_CONTAINS, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+
+        }
+
+        [Test]
+        public void STR_CONTAINS_CaseInsensitive_False_Does_Not_Match_When_Case_Differs()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "sentence", Operator.STR_CONTAINS, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A SENTENCE containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+
+        }
+
+        [Test]
+        public void STR_CONTAINS_CaseInsensitive_False_Does_Not_Match_When_String_Is_Missing()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "something", Operator.STR_CONTAINS, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+
+        }
+
+        [Test]
+        public void STR_CONTAINS_CaseInsensitive_True_Matches_When_Case_Differs()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "sentence", Operator.STR_CONTAINS, true);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A SENTENCE containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+
+        }
+
+        [Test]
+        public void STR_STARTS_WITH_CaseInsensitive_False_Matches_With_Same_Case()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "A sentence", Operator.STR_STARTS_WITH, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+
+        }
+
+        [Test]
+        public void STR_STARTS_WITH_CaseInsensitive_False_Does_Not_Match_When_Case_Differs()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "A sentence", Operator.STR_STARTS_WITH, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A SENTENCE containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+
+        }
+
+        [Test]
+        public void STR_STARTS_WITH_CaseInsensitive_False_Does_Not_Match_When_String_Is_Missing()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "something", Operator.STR_STARTS_WITH, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+
+        }
+
+        [Test]
+        public void STR_STARTS_WITH_CaseInsensitive_True_Matches_When_Case_Differs()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "a sentence", Operator.STR_STARTS_WITH, true);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A SENTENCE containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+
+        }
+
+        [Test]
+        public void STR_ENDS_WITH_CaseInsensitive_False_Matches_With_Same_Case()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "matched", Operator.STR_ENDS_WITH, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+
+        }
+
+        [Test]
+        public void STR_ENDS_WITH_CaseInsensitive_False_Does_Not_Match_When_Case_Differs()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "matched", Operator.STR_ENDS_WITH, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be MATCHED");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+
+        }
+
+        [Test]
+        public void STR_ENDS_WITH_CaseInsensitive_False_Does_Not_Match_When_String_Is_Missing()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "something", Operator.STR_ENDS_WITH, false);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeFalse();
+
+        }
+
+        [Test]
+        public void STR_ENDS_WITH_CaseInsensitive_True_Matches_When_Case_Differs()
+        {
+            // Arrange
+            var target = new StringConstraintOperator();
+            var constraint = new Constraint("operator_string_test", "matched", Operator.STR_ENDS_WITH, true);
+            var context = new UnleashContext();
+            context.Properties.Add("operator_string_test", "A sentence containing a word that should be matched");
+
+            // Act
+            var result = target.Evaluate(constraint, context);
+
+            // Assert
+            result.Should().BeTrue();
+
+        }
+    }
+}
