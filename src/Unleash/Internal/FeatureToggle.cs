127ed096c938213ec3fc46b3da6ef9e30c010c16
tomshm <tomshm@google.com>
2022-04-24 12:50:12 +0300

    Add missing description and stale properties to feature flag type

--- src/Unleash/Internal/FeatureToggle.cs
+++ src/Unleash/Internal/FeatureToggle.cs
@@ -5,25 +5,36 @@ namespace Unleash.Internal
 {
     public class FeatureToggle
     {
-        public FeatureToggle(string name, string type, bool enabled, List<ActivationStrategy> strategies, List<VariantDefinition> variants = null)
+        public FeatureToggle(
+            string name,
+            string description,
+            string type,
+            bool enabled,
+            bool stale,
+            List<ActivationStrategy> strategies,
+            List<VariantDefinition> variants = null)
         {
             Name = name;
+            Description = description;
             Type = type;
             Enabled = enabled;
+            Stale = stale;
             Strategies = strategies;
             Variants = variants ?? new List<VariantDefinition>();
         }
 
         public string Name { get; }
+        public string Description { get; }
         public string Type { get; }
         public bool Enabled { get; }
+        public bool Stale { get; }
         public List<ActivationStrategy> Strategies { get; }
 
         public List<VariantDefinition> Variants { get; }
 
         public override string ToString()
         {
-            return $"FeatureToggle{{name=\'{Name}{'\''}, enabled={Enabled}, strategies=\'{Strategies}{'\''}{'}'}";
+            return $"FeatureToggle{{name=\'{Name}{'\''}, enabled={Enabled}, stale={Stale}, strategies=\'{Strategies}{'\''}{'}'}";
         }
     }
 }
