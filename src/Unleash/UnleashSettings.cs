7202e5051eb762dfdbf81aa5548c2f1fc319b80c
Gard Rimestad <gard@getunleash.ai>
2023-02-28 11:21:59 +0100

    omg

--- src/Unleash/UnleashSettings.cs
+++ src/Unleash/UnleashSettings.cs
@@ -142,7 +142,7 @@ namespace Unleash
 
         private static string GetSdkVersion()
         {
-
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
             var version = assemblyName.Version.ToString(3);
 
             return $"unleash-client-dotnet:v{version}";
