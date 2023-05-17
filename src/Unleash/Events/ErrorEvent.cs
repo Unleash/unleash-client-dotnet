472b4150cc2212ee0a0f712023c53f15b4bb2f78
David Leek <davidekl@hotmail.com>
2023-05-17 10:46:37 +0200

    feat: add errorevent mechanism

---
+++ src/Unleash/Events/ErrorEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Unleash.Events
+{
+    public class ErrorEvent
+    {
+        public ErrorType ErrorType { get; set; }
+        public Exception Error { get; set; }
+        public HttpStatusCode? StatusCode { get; internal set; }
+        public string Resource { get; internal set; }
+    }
+}
---
+++ src/Unleash/Events/ErrorSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unleash.Events
+{
+    public enum ErrorType
+    {
+        Client,
+        TogglesBackup,
+        Bootstrap,
+        ImpressionEvent,
+        FileCache
+    }
+}
