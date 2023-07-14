cb62f39fd86a08cb8bf7ac88aeeadca65592a01c
David Leek <david@getunleash.io>
2023-07-14 08:22:28 +0200

    chore: system.net.http namespace

--- tests/Unleash.Tests/DefaultUnleashTests.cs
+++ tests/Unleash.Tests/DefaultUnleashTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
