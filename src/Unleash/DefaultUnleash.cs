3a8258725670ed3294c14c5bfb7c2265c526df15
David Leek <david@getunleash.io>
2023-08-24 11:31:30 +0200

    fix: remove accidental namespace import

--- src/Unleash/DefaultUnleash.cs
+++ src/Unleash/DefaultUnleash.cs
@@ -6,7 +6,6 @@ namespace Unleash
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Runtime.Remoting.Contexts;
     using System.Threading;
     using Unleash.Events;
     using Unleash.Variants;
