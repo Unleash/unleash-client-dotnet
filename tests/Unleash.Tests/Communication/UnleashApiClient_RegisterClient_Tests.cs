8f319a309ec9e4195a08157b6fa19e2ca0836109
David Leek <david@getunleash.io>
2023-12-07 13:59:52 +0100

    chore: add a test for content-length header

--- tests/Unleash.Tests/Communication/UnleashApiClient_RegisterClient_Tests.cs
+++ tests/Unleash.Tests/Communication/UnleashApiClient_RegisterClient_Tests.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using Unleash.Communication;
+using Unleash.Internal;
 using Unleash.Metrics;
+using Unleash.Serialization;
 
 namespace Unleash.Tests.Communication
 {
@@ -29,6 +33,51 @@ namespace Unleash.Tests.Communication
 
             var result = await api.RegisterClient(clientRegistration, CancellationToken.None);
             result.Should().Be(true);
+        }
+
+        [Test]
+        public async Task Headers()
+        {
+            var jsonSerializer = new DynamicNewtonsoftJsonSerializer();
+            jsonSerializer.TryLoad();
+            var messageHandler = new InvokingMessageHandler((message) => {
+                Assert.AreEqual(true, message.Content.Headers.Contains("Content-Length"), "Content-Length header is not set");
+                Assert.AreEqual("183", message.Content.Headers.GetValues("Content-Length").First(), "Content-Length header value mismatch");
+            });
+            var client = new HttpClient(messageHandler);
+            client.BaseAddress = new Uri("http://localhost:8080/api/");
+            var apiClient = new UnleashApiClient(client, jsonSerializer, new UnleashApiClientRequestHeaders(), new EventCallbackConfig());
+            var clientRegistration = new ClientRegistration()
+            {
+                AppName = GetType().Name,
+                InstanceId = "instance1",
+                Interval = 1000,
+                SdkVersion = "sdk101",
+                Started = DateTimeOffset.UtcNow,
+                Strategies = new List<string>
+                {
+                    "abc"
+                }
+            };
+
+            var result = await apiClient.RegisterClient(clientRegistration, CancellationToken.None);
+            result.Should().Be(true);
+        }
+    }
+
+    internal class InvokingMessageHandler : HttpMessageHandler
+    {
+        private readonly Action<HttpRequestMessage> action;
+
+        internal InvokingMessageHandler(Action<HttpRequestMessage> action)
+        {
+            this.action = action;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            action(request);
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
         }
     }
 }
