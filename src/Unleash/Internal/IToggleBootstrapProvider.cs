4ad3043ff6817ca992f7865f86c8f7db0d806134
David Leek <david@getunleash.io>
2024-09-03 15:52:11 +0200

    chore: change deprecation text to will be replaced in IToggleBootstrapProvider

--- src/Unleash/Internal/IToggleBootstrapProvider.cs
+++ src/Unleash/Internal/IToggleBootstrapProvider.cs
@@ -6,7 +6,7 @@ namespace Unleash.Internal
 {
     public interface IToggleBootstrapProvider
     {
-        [Obsolete("Will return json string in the next major version", false)]
+        [Obsolete("Will be replaced in the next major version", false)]
         ToggleCollection Read();
     }
 }
