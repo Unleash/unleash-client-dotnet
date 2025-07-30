7e20f99313d292d53d23d8ffafba1730abda224c
Simon Hornby <simon@getunleash.io>
2025-07-30 16:54:40 +0200

    fix: working etag (#289)
    * chore: test to prove that etags don't work correctly
    
    * fix: weak etags are now respected
    
    * chore: upgrade tests to work on v5

---
+++ tests/Unleash.Tests/Communication/UnleashApiClient_Features_Tests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Unleash.Communication;
+using Unleash.Internal;
+using Unleash.Tests.Mock;
+
+namespace Unleash.Tests.Communication
+{
+    public class UnleashApiClient_Features_Tests
+    {
+        private UnleashApiClient NewTestableClient(MockHttpMessageHandler messageHandler)
+        {
+            var apiUri = new Uri("http://unleash.herokuapp.com/api/");
+
+            var requestHeaders = new UnleashApiClientRequestHeaders
+            {
+                AppName = "api-test-client",
+                InstanceTag = "instance1",
+                ConnectionId = "00000000-0000-4000-a000-000000000000",
+                SdkVersion = "unleash-client-mock:0.0.0",
+                CustomHttpHeaders = null,
+                CustomHttpHeaderProvider = null
+            };
+
+            var httpClient = new HttpClient(messageHandler)
+            {
+                BaseAddress = apiUri,
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+            return new UnleashApiClient(httpClient, requestHeaders, new EventCallbackConfig());
+        }
+
+        [Test]
+        public async Task WeakEtagIsCorrectlyAddedToRequests()
+        {
+            var weakETag = "W/\"1d7-RkvEPkkxAVI06R3w4JXj3w==\"";
+            var messageHandler = new MockHttpMessageHandler();
+            messageHandler.ETagToReturn = weakETag;
+            var client = NewTestableClient(messageHandler);
+
+            var toggles = await client.FetchToggles(weakETag, CancellationToken.None);
+            toggles.HasChanged.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task StrongEtagIsCorrectlyAddedToRequests()
+        {
+            var strongETag = "\"1d7-RkvEPkkxAVI06R3w4JXj3w==\"";
+            var messageHandler = new MockHttpMessageHandler();
+            messageHandler.ETagToReturn = strongETag;
+            var client = NewTestableClient(messageHandler);
+
+            var toggles = await client.FetchToggles(strongETag, CancellationToken.None);
+            toggles.HasChanged.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task NoEtagReturnsEmptyToggles()
+        {
+            var messageHandler = new MockHttpMessageHandler();
+            var client = NewTestableClient(messageHandler);
+            var toggles = await client.FetchToggles(null, CancellationToken.None);
+            //cheating here because the SDK considers an empty response as no changes
+            toggles.Etag.Should().BeNull();
+        }
+    }
+}
--- tests/Unleash.Tests/Mock/MockHttpMessageHandler.cs
+++ tests/Unleash.Tests/Mock/MockHttpMessageHandler.cs
@@ -10,16 +10,25 @@ namespace Unleash.Tests.Mock
 {
     public class MockHttpMessageHandler : HttpMessageHandler
     {
-        public List<HttpRequestMessage> SentMessages = new List<HttpRequestMessage>();
+        public List<HttpRequestMessage> SentMessages { get; } = new List<HttpRequestMessage>();
+
+        public string? ETagToReturn { get; set; }
 
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             SentMessages.Add(request);
 
-            return Task.FromResult(new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
             {
                 Content = new StringContent("{}", Encoding.UTF8, "application/json")
-            });
+            };
+
+            if (!string.IsNullOrEmpty(ETagToReturn))
+            {
+                response.Headers.ETag = System.Net.Http.Headers.EntityTagHeaderValue.Parse(ETagToReturn);
+            }
+
+            return Task.FromResult(response);
         }
     }
 }
